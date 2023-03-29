1850dcaf427cf791f6570a3117151a64c727ed18
Christian Walpen <christianwalpen@opacc.ch>
2023-03-29 14:27:38 +0200

    Try use nuke for sonar scans

--- src/Tests/WorkflowValidation.Test/WorkflowExtensionsTests.cs
+++ src/Tests/WorkflowValidation.Test/WorkflowExtensionsTests.cs
@@ -126,7 +126,7 @@ namespace WorkflowValidation.Test
             workflow.Run();
 
             sw.Stop();
-            sw.ElapsedMilliseconds.Should().BeGreaterThan(500);
+            sw.ElapsedMilliseconds.Should().BeGreaterThanOrEqualTo(500);
         }
     }
 }
